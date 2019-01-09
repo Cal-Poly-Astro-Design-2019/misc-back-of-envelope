7923c36e2d596f9c218fd9e972230fa9d6b3f6b2
Pat Chizek <patrick.chizek@comcast.net>
2019-01-09 15:08:27 -0800

    Create material_processing_requirements.m

---
+++ material_processing_requirements.m
@@ -0,0 +1,52 @@
+%% Processing Requirements
+
+% PATRICK CHIZEK
+% 1/9/19
+
+% DESCRIPTION: Calculations for ammount of raw material that would need to 
+% be processed to extract required ammount of minerals
+
+%% Assumed Constants
+
+m.au = 643;								%[mT]
+m.pt = 93;								%[mT]
+m.pd = 105;								%[mT]
+m.ir = 129;								%[mT]
+m.ru = 50;								%[mT]
+m.rh = 12;								%[mT]
+m.ge = 61;								%[mT]
+m.ga = 97;								%[mT]
+% Mass of each mineral required
+
+m.total = sum(m);						%[mT]
+% Total mass of minerals required
+
+
+
+
+%% Asteroids
+
+% List concentrations of each mineral in each type of asteroid
+
+ast.m.conc.au = ;
+
+
+
+% M-Type
+
+
+
+% S-Type
+
+
+% Other?
+
+%% Moon
+
+
+
+%% Mars
+
+
+
+
