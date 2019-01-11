21d0c76eba6b0fe507b7b313edd8fa8d3bfe0913
Pat Chizek <patrick.chizek@comcast.net>
2019-01-11 09:44:36 -0800

    Finished calculations for S-Type based on H and LL Chondrite Meteorites

--- asteroid_material_processing.m
+++ asteroid_material_processing.m
@@ -13,17 +13,19 @@ clc;
 %% Assumed Constants
 
 m.au = 643;								%[mT]
+
 m.pt = 93;								%[mT]
 m.pd = 105;								%[mT]
 m.ir = 129;								%[mT]
 m.ru = 50;								%[mT]
 m.rh = 12;								%[mT]
-m.ge = 61;								%[mT]
 m.ga = 97;								%[mT]
+
+m.ree = 600;							%[mT]
+
 % Mass of each mineral required
 
-m.total = 1190;
-% sum(cell2mat(struct2cell(ast.l)));	%[mT]
+m.total = sum(cell2mat(struct2cell(m)));	%[mT]
 % Total mass of minerals required
 
 %% H-Chondrite Concentrations (S-Type)
@@ -38,7 +40,7 @@ ast.h.pd = 4.5;								%[ppm]
 ast.h.ir = 3.9;								%[ppm]
 ast.h.ru = 5.8;								%[ppm]
 ast.h.rh = 1.1;								%[ppm]
-ast.h.os = 5.2;
+ast.h.os = 5.2;								%[ppm]
 % PGMS
 
 ast.h.ree = 4.1;
@@ -47,6 +49,9 @@ ast.h.ree = 4.1;
 ast.h.total = sum(cell2mat(struct2cell(ast.h)));
 
 %% LL-Chondrite Concentrations (S-Type)
+
+% List concentrations of each mineral in each type of asteroid
+
 ast.l.au = 3.5;								%[ppm]
 % Gold
 
@@ -63,8 +68,6 @@ ast.h.ree = 4.1;
 
 ast.l.total = sum(cell2mat(struct2cell(ast.l)));
 
-% Other?
-
 %% Ton of raw material to Ton of Refined Material
 
 ast.h.raw = 1000000/ast.h.total;
@@ -75,8 +78,11 @@ ast.l.raw = 1000000/ast.l.total;
 % Tons of LL-Chondrite asteroid required to be processed to yield 1 ton of
 % material
 
-disp(ast.h.raw);
-disp(ast.l.raw);
+
+disp('Tonnes of Asteroid Material to Yield 1 tonne of Minerals');
+disp(['H-Chondrite ',num2str(ast.h.raw),' tonnes per tonne of useful material.']);
+disp(['LL-Chondrite ',num2str(ast.l.raw),' tonnes per tonne of useful material.']);
+fprintf('\n');
 
 %% Tons of raw material to be processed per year
 
@@ -88,7 +94,6 @@ ast.l.year = ast.l.raw*m.total;
 % Tons of LL-Chondrite asteroid required to be processed to yield 1 ton of
 % material
 
-disp(ast.h.year);
-disp(ast.l.year);
-
-
+disp('Tonnes of Asteroid Material to be Processed to Meet Requirements');
+disp(['H-Chondrite ',num2str(ast.h.year),' tonnes per year.']);
+disp(['LL-Chondrite ',num2str(ast.l.year),' tonnes per year.']);
